9762ddde2e0ea5ff5447a699e5eaf405f6c2f0c5
Thiago Silva <thiago.silva@hotmail.com>
2019-07-29 16:05:00 -0300

    alteracoes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/home'
-import axios from 'axios'
 
 
 class App extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      repos: [], 
-      total_count: 0
-    } 
-  }
-
-  getRepositories(){
-    axios.get('https://api.github.com/users/thg021/repos',{
-      params: {
-        sort: 'created' 
-      }
-    })
-    .then(({ data }) => this.setState({repos: data }))
-    .catch(e => {
-      console.error('Deu ruim', e)
-    })
-  }
-
-  componentDidMount(){
-    this.getRepositories()
-  }
-
   render() {
-    const { repos } = this.state
-    console.log(repos)
      return (
       <div className="App">
-        <div>
-          <p>Primeira chamadas</p>
-        
-            {console.log(this.state.repos)}
-            {
-              repos.map((item, key) => <li key={key}>{item.name}</li>)
-            }
-          
-        </div>
+        <Home />
 
       </div>
     );
--- src/components/home.js
+++ src/components/home.js
@@ -1,30 +1,127 @@
 import React, { Component } from 'react'
-import Request from '../rest/Request'
+import api from '../services/api'
+import styled from 'styled-components'
+
+const RepositoryList = styled.div`
+    max-width: 700px;
+    margin: 20px auto 0;
+    padding: 0 20px;
+`
+
+const Article = styled.article`
+    background: #fff; 
+    border: 1px #ddd solid; 
+    border-radius: 5px;
+    padding: 20px;
+    margin-bottom: 20px;
+`
+
+const CreatedDate = styled.p`
+    font-size: 16px;
+    color: #999;
+    margin-top: 5px 0;
+    line-height: 24px;
+`
 
 
 class Home extends Component {
 
+   state = {
+        repository: []
+    }
+
+    componentDidMount(){
+        this.loadRepositories()
+    }
+
+    loadRepositories = async () => {
+        try {
+            const response = await api.get('/users/thg021/repos',{
+                params: {
+                  sort: 'created'
+                }
+              })
+             this.setState({repository: response.data})
+
+              
+
+        } catch (error) {
+            console.log(error)
+        }
+
+       
+    }
+
+    getTotalRepositoryYear = (repository) => {
+        let repositoryDate = repository.map(item => {      
+            return item.created_at.slice(0,4)
+        }).reduce((obj, item) => {
+            if ( !obj[item] ) {
+                obj[item]=1;
+             }else{
+                 obj[item]++
+             }
+            return obj
+        },{})
+        for(let teste in repositoryDate){
+           console.log('Aqui',teste, repositoryDate[teste])       
+        }
+     
+        return repositoryDate
+    }
+
+    listRepositoryByYear = (key) => {
+        let repos = this.state.repository.filter(item => item.created_at.slice(0,4) === key )
+        return(
+            repos.map(item => (
+                <Article key={item.id}>
+                    <strong>{item.name}</strong>
+                    <CreatedDate>{item.created_at}</CreatedDate>
+                </Article>
+            ))
+        )
+    } 
+
+  
     render(){
+        const repository = this.state.repository
+      
+        const yearsRepository = repository.length ?  this.getTotalRepositoryYear(repository) : ''
+
         return (
-            <div>
-               <Request  url={'https://api.github.com/users/thg021/repos'} config={ {method:"get", params: {
-        sort: 'created', 
-        direction: 'asc'
-      }} } render={ response => 
-                    {
-                        
-                        console.log(response.data)
-                        return (
-                            <div className="grid-cards">
-                                <ul>
-                                    {response.data.map((item, key) => <li key={key}>{item.name}</li>)}
-                                </ul>
-                            </div>
-                        )
-                    }
-                } />
+            <RepositoryList>
                 
-            </div>
+                {
+                    Object.keys(yearsRepository).map(key => (
+                        <div key={key}>  
+                            <strong>{key}</strong>
+                            <p>{yearsRepository[key] <= 1 ?  `${yearsRepository[key]} Repositório criado` : `${yearsRepository[key]} Repositórios criados.`}</p>
+                        </div>
+                    )).reverse()
+                }
+
+{
+                    Object.keys(yearsRepository).map(key => (
+                        <div key={key}>  
+                            <strong>{key}</strong>
+                           {
+                               this.listRepositoryByYear(key)
+                           }
+                        </div>
+                    )).reverse()
+                }
+                            
+                {/*
+                   
+                    repository.map(item => (
+                        <Article key={item.id}>
+                            <strong>{item.name}</strong>
+                            <CreatedDate>{item.created_at}</CreatedDate>
+                        </Article>
+                        
+                    ))*/
+                }
+            </RepositoryList>
         )
     }
 }
